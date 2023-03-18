c39fe181c8a5d36aa3da30b1114b2aa2680dca52
clipKoala <s.jozwiak@htlstp.at>
2023-03-18 20:38:47 +0100

    test commit

---
+++ lib/domain/Subject.dart
@@ -0,0 +1,22 @@
+class Subject {
+  final String name;
+  int time;
+
+  Subject(this.name, {this.time = 0});
+
+  @override
+  String toString() {
+    return 'Subject{name: $name, time: $time}';
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is Subject &&
+          runtimeType == other.runtimeType &&
+          name == other.name &&
+          time == other.time;
+
+  @override
+  int get hashCode => name.hashCode ^ time.hashCode;
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_02_learning_app/domain/Subject.dart';
 
 void main() {
   runApp(const LearningApp());
@@ -9,6 +10,73 @@ class LearningApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      home: HomePage(),
+    );
+  }
+}
+
+class HomePage extends StatefulWidget {
+  HomePage({Key? key}) : super(key: key);
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var subjects = <Subject>[];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _showInput(context),
+        child: const Icon(Icons.add),
+        backgroundColor: Colors.green,
+      ),
+      appBar: AppBar(
+        leading: Icon(Icons.school),
+        title: const Text('My subjects'),
+        backgroundColor: Colors.green,
+      ),
+      body: Column(
+        children: [
+          Flexible(
+            flex: 1,
+            child: Container(
+              color: Colors.blueGrey,
+            ),
+          ),
+          Flexible(
+            flex: 1,
+            child: Container(
+              color: Colors.blueGrey,
+              padding: const EdgeInsets.all(8.0),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  _showInput(BuildContext context) {
+    showModalBottomSheet(
+      context: context,
+      builder: (context) {
+        return Column(
+          children: [
+            TextField(
+              onSubmitted: (value) {
+                var subject = Subject(value);
+                setState(() {
+                  subjects.add(subject);
+                });
+                Navigator.pop(context);
+              },
+            ),
+          ],
+        );
+      },
+    );
   }
 }
